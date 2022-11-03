735913b0e0e45e42e96c45a9e9681e3aee9db9ea
marvin-0 <alsgh3724@gmail.com>
2022-11-03 15:25:46 +0900

    drill-12 완성
    2019182039 최민호

---
+++ drill_12/grass.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self, x = 400, y = 30):
+        self.image = load_image('grass.png')
+        self.x, self.y = x, y
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        pass
+
+
